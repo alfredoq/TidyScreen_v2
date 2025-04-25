a4b876fdeb2e27e3d86ea9f0c790d0ebd105e862
Alfredo Quevedo <aquevedo@unc.edu.ar>
2025-04-25 13:24:02 -0300

    Modify setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='tidyscreen',
-      version='0.0.9',
+      version='0.1.0',
       description='A library to manage data associated to drug screening campaigns',
       long_description=open('README.md').read().strip(),
       author="MedChemLab staff",
