663ef776cfab80f3f3fe6ad964f7eb03cec6aee0
Alfredo Quevedo <aquevedo@unc.edu.ar>
2025-05-29 14:41:29 -0300

    Update README file with installation instructions

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='tidyscreen',
-      version='0.2.2',
+      version='0.2.3',
       description='A library to manage data associated to drug screening campaigns',
       long_description=open('README.md').read().strip(),
       author="MedChemLab staff",
